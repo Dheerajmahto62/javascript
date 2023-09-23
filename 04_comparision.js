8cf9b74f3ac2ae664bbeef600b204583c9f3dcbb
Dheeraj kumar mahto <126415291+Dheerajmahto62@users.noreply.github.com>
2023-09-23 13:49:00 +0530

    comparision datatype

---
+++ 04_comparision.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+console.log("2"> 1)
+console.log("02" > 1)
+
+console.log("**************")
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log("**************")
+console.log(undefined > 0)
+console.log(undefined == 0 )
+console.log(undefined < 0)
+
+
+//  ===
+console.log("**************")
+console.log("5" === "5")
+console.log("2" === 3)  // its check datatype and values should  be both are same then output is true otherwise false
