0aba90d8c6c6b486d3ac7b65ecdae5041ab6d389
Dheeraj kumar mahto <126415291+Dheerajmahto62@users.noreply.github.com>
2023-09-23 13:23:48 +0530

    Operetion

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -24,8 +24,39 @@
 
 
 
-let someNumber = 20
+// let someNumber = 20
 
-let stringNumber= String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber= String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//  *****************   Operation    ***********************
+
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1 = "Hello"
+// let str2 = " Dheeraj"
+
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1+ "2" + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(3 + 4 * 5 % 3)
+// console.log((3 + 4) * 5 % 3)
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter)
