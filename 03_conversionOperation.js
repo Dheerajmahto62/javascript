b912a5cdcea3f3dda77953bb144b487e969f3c50
Dheeraj kumar mahto <126415291+Dheerajmahto62@users.noreply.github.com>
2023-09-23 12:05:11 +0530

    Conversion of datatype

---
+++ 03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = "33asd"
+// let score = "33"
+// let score = null
+// let score = undefined
+// let score = false + true;
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+
+// let number = 1
+// let number = "Dheeraj"
+// let number = 0
+
+
+// let bool = Boolean(number)
+// console.log(typeof bool)
+// console.log(bool)
+
+
+
+
+let someNumber = 20
+
+let stringNumber= String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
