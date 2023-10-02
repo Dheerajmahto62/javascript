7e98232ab5ddc4215b85f1d841e8cba9fc436d11
Dheeraj kumar mahto <126415291+Dheerajmahto62@users.noreply.github.com>
2023-10-02 16:14:08 +0530

    change

--- Array/02_array.js
+++ Array/02_array.js
@@ -23,4 +23,4 @@ let score1 =100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1,score2,score3)) // value store in Array
+console.log(Array.of(score1,score2,score3)) // value store in Arraychange
