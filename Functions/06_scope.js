b12657448d3735621724f98b6c5524f5e873a21d
Dheeraj kumar mahto <126415291+Dheerajmahto62@users.noreply.github.com>
2023-10-03 21:14:06 +0530

    Scopes

---
+++ Functions/06_scope.js
@@ -0,0 +1,13 @@
+// var b = 200
+// let a = 300
+if(true) {
+    let a = 10 
+    const b = 20
+    // var c = 30
+
+    console.log("Inner : ", a)
+}
+
+console.log(a)
+console.log(b)
+// console.log(c)
