ce597b2a633642d6a8855b13992ca1e3dcf098fe
Dheeraj kumar mahto <126415291+Dheerajmahto62@users.noreply.github.com>
2023-10-03 20:52:04 +0530

    objects

---
+++ Objects/07_object.js
@@ -0,0 +1,15 @@
+
+
+const course = {
+    coursename: "JAVASCRIPT",
+    price: "999",
+    courseInstructor: "Dheeraj"
+}
+
+// course.courseInstructor
+const {courseInstructor: Instructor} = course
+// console.log(courseInstructor)
+console.log(Instructor)
+
+
+// ------------ Object Literals ------
